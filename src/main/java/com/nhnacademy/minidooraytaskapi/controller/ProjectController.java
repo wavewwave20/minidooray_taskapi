0fa93a3b255ec7673cdf64b23b3ade337d576d18
nemzy <wavewwave20@gmail.com>
2023-06-11 19:08:52 +0900

    init

--- src/main/java/com/nhnacademy/minidooraytaskapi/controller/ProjectController.java
+++ src/main/java/com/nhnacademy/minidooraytaskapi/controller/ProjectController.java
@@ -1,9 +1,16 @@
 package com.nhnacademy.minidooraytaskapi.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/taskapi/projects")
 public class ProjectController {
+
+    @PostMapping("/create")
+    public void createProject( ) {
+
+    }
 }
